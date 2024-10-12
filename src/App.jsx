7820bf7f67a51ff7e1d183514a569b00d27c37c9
nafistarik <nafistarik789@gmail.com>
2024-10-12 17:40:10 +0600

    project setup done

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <h1 className="text-5xl text-white font-bold h-lvh bg-sky-400 flex justify-center items-center">
+      Tic Tac Toe
+    </h1>
+  );
+}
